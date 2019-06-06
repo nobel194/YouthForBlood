082af11b9a11afa16c7199390eb1d5072716ddeb
Nobel <hollow.sky2@gmail.com>
2019-06-06 23:02:04 +0545

    Changes for CRUD operation

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-var bodyParser = require('body-parser');
-var express = require('express');
+const bodyParser = require('body-parser');
+const express = require('express');
 
 const app = express();
 const db = require('./database.js');
@@ -8,7 +8,8 @@ const routes = require('./route.js');
 app.use(bodyParser.json());
 app.use(express.static('blood'), { index : false} ); //Serves resources from folder blood
 
-app.use('/', routes)
+app.use(routes);
+
 db.on('error', function(err) {console.log('Database connection error',err)});
 db.once('connected', function() {
   console.log("connected to mongodb!");
