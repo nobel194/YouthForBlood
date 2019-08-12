a80c508cb9fe18602f89f8f7c26cb2e37067b387
Nobel <hollow.sky2@gmail.com>
2019-08-12 10:15:22 +0545

    admin panel added

--- server.js
+++ server.js
@@ -1,20 +1,82 @@
-const bodyParser = require('body-parser');
-const express = require('express');
+//express server
+require('./models/db');
+var express= require('express');
+const exphbs = require('express-handlebars');
+var app= express();
+const employeeController = require('./controller/employeeController');
+//schema required
+var register = require('./schemas/registration_schema');
+var nearby= require('./schemas/donorsnear_schema');
+var Event = require('./schemas/event');
+const adminRouter = require('./models/admin.router.js');
+//mongoose connection
+var mongoose= require('mongoose');
+mongoose.Promise=global.Promise;
+mongoose.connect("mongodb://localhost:27017/test", { useNewUrlParser: true });
 
-const app = express();
-const db = require('./database.js');
-const routes = require('./route.js');
+//View Engine
+app.set("view engine", "hbs");
 
-app.use(bodyParser.json());
-app.use('/',express.static(__dirname + '/blood/public', { index : false})); //Serves resources from folder blood
 
-app.use(routes);
+//body parser 
+var bodyparser= require('body-parser');
+app.use(bodyparser.urlencoded({ extended:true }));
+app.use('/admin', adminRouter);
 
-db.on('error', function(err) {console.log('Database connection error',err)});
-db.once('connected', function() {
-  console.log("connected to mongodb!");
+//path 
+var path= require('path');
 
-  app.listen(5000,function(){
-	console.log('Server running at localhost:'+5000);
-  })
+//middleware
+app.use(express.static(path.join(__dirname,"public")));
+
+//var router= require('./route1');
+//app.use(router);
+
+//router yo home page
+var home_routes= require('./home_route');
+app.use(home_routes);
+
+var login = require('./controller/login');
+app.use('/login', login);
+//router to donor registration
+var reg_routes= require('./registration_route');
+app.use(reg_routes);
+//router to donors nearby
+var donornearby_routes= require('./donors_nearby');
+app.use(donornearby_routes);
+//router to events//
+var event_route = require('./event_route');
+app.use('/event',event_route);
+//router for admin panel
+app.use('/employee', employeeController);
+app.use(bodyparser.json());
+app.set('views', path.join(__dirname, '/views/'));
+app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
+app.set('view engine', 'hbs');
+
+app.get('/event/add',function(req,res){
+    res.render('event')
 });
+
+app.get('/event/:id', async(req, res)=>{
+    let eventid = await Event.findOne({_id:req.params.id});
+    eventid.isApproved = true;
+    eventid.save();
+    return res.render('success');
+});
+
+/*app.get('/registredUsers', (req, res) => {
+    register.find({}, (err, data) => {
+        //find{ name: req.name}
+        // data = data[6];
+        console.log(data);
+        // res.render("1", { name: data.name,age:data.age,gender:data.gender, fileName: data.imageLocation });
+        res.render('1', {data: data})
+    });
+    
+})*/
+
+app.listen(3000, () => {
+    console.log('Express server started at port : 3000');
+});
+
